41ced724eaf9453d1b45614e3273e49a34e4a4b9
RayToth <t.rajmund.98@gmail.com>
2020-05-25 14:49:46 +0200

    Create search form

--- gamecodex/src/components/layout/Navbar.js
+++ gamecodex/src/components/layout/Navbar.js
@@ -8,8 +8,19 @@ const Link = styled.h4`
 
 const Navbar = (props) => {
   return (
-    <div className="navbar navbar-expand-md bg-dark fixed-top">
+    <div className="navbar navbar-expand-md bg-dark fixed-top justify-content-between">
       <Link>Games</Link>
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        ></input>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
