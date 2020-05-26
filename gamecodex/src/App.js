0f7da904b9acc9617fd33cac0988d9b7363759e6
RayToth <t.rajmund.98@gmail.com>
2020-05-26 10:54:32 +0200

    Create container for card list

--- gamecodex/src/App.js
+++ gamecodex/src/App.js
@@ -4,13 +4,22 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  width: 80%;
+  background-color: #202121;
+  border-radius: 15px;
+  margin: auto;
+  margin-top: 10px;
+`;
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <div className="container"></div>
+        <CardContainer>test</CardContainer>
       </div>
     </BrowserRouter>
   );
