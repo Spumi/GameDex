37e3b966251d002f0d51237defef06e14ff5a145
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-05-28 14:12:13 +0200

    added gameContext file

--- gamecodex/src/gameContext.js
+++ gamecodex/src/gameContext.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 const GameContext = React.createContext({ gameId: null });
-export const PageProvider = GameContext.Provider;
+export const GameProvider = GameContext.Provider;
 export default GameContext;
