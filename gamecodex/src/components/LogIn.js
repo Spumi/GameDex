63dc1118cbfe0c5556042998f083516cf8846a32
RayToth <t.rajmund.98@gmail.com>
2020-06-11 14:38:41 +0200

    Update LogIn.js

--- gamecodex/src/components/LogIn.js
+++ gamecodex/src/components/LogIn.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const CenterDiv = styled.div`
   margin: 0 auto;
@@ -16,42 +17,67 @@ const StyledLabel = styled.label`
   color: lightgray;
 `;
 
-const StyledButton = styled.button`
+const StyledInput = styled.input`
   margin-top: 30px;
   width: 50%;
   align-self: center;
 `;
+
+const StyledForm = styled.form`
+  margin: auto;
+`;
+
+const LoginUser = (event) => {
+  event.preventDefault();
+  const user = {
+    Username: document.getElementsByClassName("username")[0].value,
+    Password: document.getElementsByClassName("password")[0].value,
+  };
+  axios
+    .post("https://localhost:44363/api/login", user)
+    .then((response) => {
+      if (response.data.username === null) {
+        window.alert("There is no account with this username/password.");
+      }
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 function LogIn(props) {
   return (
     <React.Fragment>
       <CenterDiv className="card">
         <StyledH3>Login</StyledH3>
-        <StyledLabel>
-          <b>Username</b>
-        </StyledLabel>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          name="username"
-          required
-        ></input>
-        <StyledLabel>
-          <b>Password</b>
-        </StyledLabel>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-        ></input>
-        <StyledButton
-          type="submit"
-          className="signupbtn"
-          onClick="LoginRequest"
-        >
-          Login
-        </StyledButton>
+        <StyledForm onSubmit={(event) => LoginUser(event)}>
+          <StyledLabel>
+            <b>Username</b>
+          </StyledLabel>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="Username"
+            className="username"
+            required
+          ></input>
+          <StyledLabel>
+            <b>Password</b>
+          </StyledLabel>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="Password"
+            className="password"
+            required
+          ></input>
+          <StyledInput
+            type="submit"
+            className="signupbtn"
+            value="Login"
+          ></StyledInput>
+        </StyledForm>
       </CenterDiv>
     </React.Fragment>
   );
